3fcf43a4643589058601c1bbb991fb7a04623d7f
Marcel Schlidt <marcels9519@web.de>
2022-12-12 20:26:06 +0100

    Background image works!

--- frontend/tw-app/src/app/app.component.ts
+++ frontend/tw-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tw-app';
+export class AppComponent implements OnInit{
+
+  title? = 'tw-app';
+  
+  /**
+   * 
+   */
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
 }
