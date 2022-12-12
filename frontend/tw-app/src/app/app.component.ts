453ecd201bb341f93d1e697f9ca867ec9056e2bf
Marcel Schlidt <marcels9519@web.de>
2022-12-12 21:38:49 +0100

    Adapt Body css

--- frontend/tw-app/src/app/app.component.ts
+++ frontend/tw-app/src/app/app.component.ts
@@ -1,19 +1,28 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { fromEvent, Subscription  } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy {
+  clientWidth?: number;
+  clientHeight?: number;
+  resizeSubscription? : Subscription;
 
-  title? = 'tw-app';
-  
   /**
-   * 
+   *
    */
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    const resizeObservable = fromEvent(window, 'resize');
+    this.resizeSubscription = resizeObservable.subscribe((event) => {
+      this.clientWidth = document.documentElement.clientWidth;
+      this.clientHeight = document.documentElement.clientHeight;
+    });
   }
 
+  ngOnDestroy(): void {
+    this.resizeSubscription?.unsubscribe();
+  }
 }
