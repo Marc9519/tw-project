4398dfda67cc52ebcbd53ba78ea31b57f717a60a
Marcel Schlidt <marcels9519@web.de>
2022-12-14 16:23:19 +0100

    Sanitize Absolute Paths

--- backend/tw-client-starter/src/main/java/org/tw/client/component/ImageResolver.java
+++ backend/tw-client-starter/src/main/java/org/tw/client/component/ImageResolver.java
@@ -10,10 +10,16 @@ import java.util.Map;
 import javax.imageio.ImageIO;
 
 public class ImageResolver {
-	
+
 	public Map<String, BufferedImage> resolveImage() throws IOException {
-		BufferedImage right = ImageIO.read(new File(
-				"C:\\Users\\a890202\\dev\\repos\\tw-project\\frontend\\tw-app\\src\\assets\\topbutton-right.png"));
+		Map<String, BufferedImage> result = new HashMap<>();
+		processRightImage(result);
+		processLeftImage(result);
+		return result;
+	}
+
+	private void processRightImage(Map<String, BufferedImage> result) throws IOException {
+		BufferedImage right = ImageIO.read(new File("./../../frontend/tw-app/src/assets/topbutton-right.png"));
 		int height = right.getHeight() / 2;
 		BufferedImage topRight = new BufferedImage(right.getWidth(), height, BufferedImage.TYPE_INT_ARGB);
 		BufferedImage bottomRight = new BufferedImage(right.getWidth(), height, BufferedImage.TYPE_INT_ARGB);
@@ -21,13 +27,11 @@ public class ImageResolver {
 		g.drawImage(right, 0, 0, null);
 		g = bottomRight.getGraphics();
 		g.drawImage(right.getSubimage(0, height, right.getWidth(), height), 0, 0, null);
-		Map<String, BufferedImage> result = new HashMap<>();
+
 		result.put("TOP_RIGHT", topRight);
 		result.put("BOTTOM_RIGHT", bottomRight);
-		processLeftImage(result);
-		return result;
 	}
-	
+
 	public BufferedImage mergeTop(BufferedImage left, BufferedImage right) {
 		BufferedImage result = new BufferedImage(left.getWidth() + right.getWidth(), left.getHeight(),
 				BufferedImage.TYPE_INT_ARGB);
@@ -36,7 +40,7 @@ public class ImageResolver {
 		g.drawImage(right, left.getWidth() - 2, 0, null);
 		return result;
 	}
-	
+
 	public BufferedImage mergeBottom(BufferedImage left, BufferedImage right) {
 		BufferedImage result = new BufferedImage(left.getWidth() + right.getWidth(), left.getHeight(),BufferedImage.TYPE_INT_ARGB);
 		Graphics g = result.getGraphics();
@@ -46,8 +50,7 @@ public class ImageResolver {
 	}
 
 	private void processLeftImage(Map<String, BufferedImage> result) throws IOException {
-		BufferedImage left = ImageIO.read(new File(
-				"C:\\Users\\a890202\\dev\\repos\\tw-project\\frontend\\tw-app\\src\\assets\\topbutton-left.png"));
+		BufferedImage left = ImageIO.read(new File("./../../frontend/tw-app/src/assets/topbutton-left.png"));
 		int height = left.getHeight() / 2;
 		BufferedImage topLeft = new BufferedImage(left.getWidth(), height, BufferedImage.TYPE_INT_ARGB);
 		Graphics g = topLeft.getGraphics();
@@ -55,7 +58,7 @@ public class ImageResolver {
 		BufferedImage bottomLeft = new BufferedImage(left.getWidth(), height, BufferedImage.TYPE_INT_ARGB);
 		g = bottomLeft.getGraphics();
 		g.drawImage(left.getSubimage(0, height, left.getWidth(), height), 0, 0, null);
-		
+
 		result.put("TOP_LEFT", topLeft);
 		result.put("BOTTOM_LEFT", bottomLeft);
 	}
